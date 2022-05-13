7744ebdebf11166ab3696378a13385a602be6e22
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-13 03:22:39 -0700

    added Part-time employee and wages

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -13,6 +13,8 @@ namespace EmployeeWageComputation
                 Console.WriteLine("\nEnter the choice");
                 Console.WriteLine("1. Check Employee is present is OR Absent");
                 Console.WriteLine("2. Calculate daily Employee Wages");
+                Console.WriteLine("3. Part time Employee and Wages");
+                Console.WriteLine("0: Exit");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -23,6 +25,10 @@ namespace EmployeeWageComputation
                     case 2:
                         DailyEmpWage dailyEmpWage = new DailyEmpWage();
                         dailyEmpWage.DailyWages();
+                        break;
+                    case 3:
+                        PartTimeEmp partTimeEmp = new PartTimeEmp();
+                        partTimeEmp.Parttime();
                         break;
                 }
             }while(choice != 0);
