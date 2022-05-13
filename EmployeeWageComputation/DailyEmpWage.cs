4e1955a2691734f0722dac90ea0dc6cd898e1e34
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-13 01:47:18 -0700

    Daily employee wage

--- EmployeeWageComputation/DailyEmpWage.cs
+++ EmployeeWageComputation/DailyEmpWage.cs
@@ -21,10 +21,12 @@ namespace EmployeeWageComputation
             int empCheck = random.Next(0,2);
             if (empCheck == IS_FULL_TIME)
                 {
+                Console.WriteLine("Employee is Present");
                 empHrs = 8;
             }
             else
             {
+                Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
