d5dfc36beab19f525d559bcec885da78d0356cd0
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-13 01:14:36 -0700

    Daily employee Wage

---
+++ EmployeeWageComputation/DailyEmpWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class DailyEmpWage
+    {
+        public void DailyWages()
+        {
+            //constants
+            const int IS_FULL_TIME = 1;
+            const int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0,2);
+            if (empCheck == IS_FULL_TIME)
+                {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,9 +6,26 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] arg)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmpPresentAbsent empPresentAbsent = new EmpPresentAbsent();
-            empPresentAbsent.PresentAbsent(); 
+            int choice;
+            do
+            {
+                Console.WriteLine("\nWelcome to Employee Wage Computation Program");
+                Console.WriteLine("\nEnter the choice");
+                Console.WriteLine("1. Check Employee is present is OR Absent");
+                Console.WriteLine("2. Calculate daily Employee Wages");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        EmpPresentAbsent empPresentAbsent = new EmpPresentAbsent();
+                        empPresentAbsent.PresentAbsent();
+                        break;
+                    case 2:
+                        DailyEmpWage dailyEmpWage = new DailyEmpWage();
+                        dailyEmpWage.DailyWages();
+                        break;
+                }
+            }while(choice != 0);
         }
     }
 }
